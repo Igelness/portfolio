15bee7649d8449fa43c9dc4555444d4fc333e2a9
Anton Vorobyov <igelness@gmail.com>
2019-11-04 20:52:48 +0300

    ADD scrollup btn, adaptive main page, cross main

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,23 @@ $(function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
 
+$(window).on("hashchange", function(e) {
+  history.replaceState("", document.title, e.originalEvent.oldURL);
+});
 
+$(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 150) {
+      $(".scrollbtn").css("opacity", "1");
+    } else {
+      $(".scrollbtn").css("opacity", "0");
+    }
+  });
+});
+
+$(function() {
+  $(".header__mobile-btn").on("click", function() {
+    $(".header__contacts").toggle();
+    $(".header__menu").toggle();
+  })
+});
